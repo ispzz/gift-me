f85ded39721297f57f0cb4c7893f077f5bae1b6e
Zara <zara@okaayfinee.local>
2020-05-15 23:19:04 +1000

    Simple show page

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="wrapper">
-    <h1 class="title">Something I would love !</h1>
-    <h4 class="title is-4">{{wishlist.name}}</h4>
     <img :src="wishlist.image" />
+
+    <div class="card">
+  <header class="card-header">
+    <p class="card-header-title is-centered">
+      {{wishlist.name}}
+    </p>
+  </header>
+  <div class="card-content">
+    <div class="content">
+      I would really love this item !
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a class="card-footer-item">
+      <router-link :to="{ name: 'edit', params: { id: wishlist.id } }">
+        Edit
+      </router-link>
+    </a>
+  </footer>
+</div>
   </div>
 </template>
 
@@ -48,5 +66,15 @@
   align-items: center;
   justify-content: center;
   flex-direction: column;
+}
+img {
+  max-height: 50vh;
+  border-radius: 1em;
+}
+h4 {
+  margin-top: 1em;
+}
+.card {
+  margin: 2em;
 }
 </style>
